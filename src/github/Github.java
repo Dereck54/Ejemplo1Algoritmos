5dbf88dfcea85964df58e3d5cf80e375d97e07b6
Luis Villatoro <Luis Villatoro@192.168.84.61>
2025-08-02 17:03:57 -0600

    "cambios 02/08/2025"

---
+++ src/github/Github.java
@@ -0,0 +1,16 @@
+package github;
+
+import java.util.Scanner;
+
+public class Github {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre,edad;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un nombre");
+        nombre=sc.nextLine();
+        System.out.println("Ingrese un nombre");
+        edad=sc.nextLine();   
+    }
+    
+}
